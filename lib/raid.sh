4f0a2919844caa5bfa37f8e4a049878196750962
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.de>
2016-07-06 14:36:01 +0200

    Fix drac model logic check

--- lib/raid.sh
+++ lib/raid.sh
@@ -3,9 +3,9 @@
 # Please note that racadm returns empty lines and \r on the next line.
 # This is why we put each command via tr and awk to get proper strings.
 
-if [ "$model" = "iDRAC7" ] || [ "$model" = "iDRAC8" ]; then
+if ! ( [ "$model" = "iDRAC7" ] || [ "$model" = "iDRAC8" ] ); then
 	echo "Raid configuration possible only on PowerEdge [RM]6[23]0 (or rather iDRAC [78])."
-	continue
+	exit 1
 fi
 
 CONTROLLER=`$racadm raid get controllers | tr -d '\r' | grep RAID.Integrated | head -n1 | awk '{print $1}'`
