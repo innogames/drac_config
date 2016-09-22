7d23cc3be8e9e2724b1b1373483714035eccc654
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.de>
2016-09-22 16:05:59 +0200

    Add new functions for SEL

---
+++ lib/sel.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Please note that racadm returns empty lines and \r on the next line.
+# This is why we put each command via tr and awk to get proper strings.
+
+case "$1" in
+	"getsel")
+		$racadm getsel
+		;;
+	*)
+		echo "Unknown action $1"
+	;;
+esac
+
