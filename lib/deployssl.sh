d1bdcb1c7ca76cd6c8406a2c746835f1cca875e4
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.de>
2017-12-29 11:48:09 +0100

    Start work on ssl key deployment

--- lib/deployssl.sh
+++ lib/deployssl.sh
@@ -2,6 +2,15 @@
 
 #$racadm set iDrac.Security.CsrKeySize 4096
 
-$racadm sslkeyupload  -t 1 -f cert/key.pem
-$racadm sslcertupload -t 1 -f cert/bundle.pem
+case "$model" in
+	iDRAC6)
+        tf=$(mktemp)
+        echo "[cfgRacSecurityData]" >> $tf
+        echo "cfgRacSecCsrIndex=1" >> $tf
+        echo "cfgRacSecCsrCommonName=$host" >> $tf
+        $racadm config -f $tf
+        rm $tf
 
+        $racadm sslcsrgen -g -f /tmp/$host.req
+	;;
+esac
