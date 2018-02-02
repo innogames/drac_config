704dab7c781d8f4aca0439541482eeb009674e7c
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.de>
2018-02-02 18:25:03 +0100

    Fix viewer on iDRAC 7 and 8

--- lib/view.sh
+++ lib/view.sh
@@ -1,34 +1,21 @@
 #!/bin/sh
 
-source=""
+cj=$(mktemp)
+jnlp=$(mktemp)
 case "$model" in
 	iDRAC6)
-		cookie=`curl -k --data "WEBVAR_USERNAME=${USER}&WEBVAR_PASSWORD=${PASS}&WEBVAR_ISCMCLOGIN=0" https://${host}/Applications/dellUI/RPC/WEBSES/create.asp 2>/dev/null | sed -rn '/SESSION_COOKIE/ s/.*SESSION_COOKIE'\'' : '\''([a-zA-Z0-9]+)'\''.*/\1/p'`
-		if [ -z "$cookie" ]; then
-			echo "Unable to get login cookie from ${host}"
-			exit;
-		fi
-	curl -s -k --cookie "Cookie=SessionCookie=$cookie" https://${host}/Applications/dellUI/Java/jviewer.jnlp -o /tmp/$host.jnlp
-		source=/tmp/$host.jnlp
-		useorig=yes
+		cookie=$(curl --cookie-jar $cj --cookie $cj -s -k --data "WEBVAR_USERNAME=${USER}&WEBVAR_PASSWORD=${PASS}&WEBVAR_ISCMCLOGIN=0" https://${host}/Applications/dellUI/RPC/WEBSES/create.asp | sed -rn '/SESSION_COOKIE/ s/.*SESSION_COOKIE'\'' : '\''([a-zA-Z0-9]+)'\''.*/\1/p')
+		curl --cookie-jar $cj --cookie $cj -s -k -o $jnlp --cookie "Cookie=SessionCookie=$cookie" "https://${host}/Applications/dellUI/Java/jviewer.jnlp"
 		;;
-	iDRAC7)
-		source=$libdir/drac7.jnlp
-		useorig=no
-		;;
-	iDRAC8)
-		source=$libdir/drac8.jnlp
-		useorig=no
+	iDRAC[78])
+		ST1=$(curl --cookie-jar $cj --cookie $cj -s -k --data "user=${USER}&password=${PASS}" https://$host/data/login | sed -E 's/.*ST1=([a-z0-9]+),.*/\1/g')
+		curl --cookie-jar $cj --cookie $cj -s -k -o $jnlp "https://$host/viewer.jnlp($host@0@$host@123@ST1=$ST1)'"
 		;;
 	*)
 		echo "Can not view $host because of its hardware model '$model'!"
 	 ;;
 esac
-if [ -n "$source" ]; then
-	echo "$host is $model, using $source"
-	if [ "$useorig" = "no" ]; then
-		sed "s/USER/$USER/; s/PASS/$PASS/; s/IPADDR/${host}/; s/HOSTNAME/$host/;" $source > /tmp/$host.jnlp
-	fi
-	javaws /tmp/$host.jnlp &
-fi
-
+rm $cj
+#echo $jnlp
+#cat $jnlp
+javaws $jnlp &
