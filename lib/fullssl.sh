7610e88c8b49abeb6dffa24d7ed22cf3052c040d
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2019-01-28 14:55:59 +0100

    Add new function: fullssl

---
+++ lib/fullssl.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+case "$model" in
+	iDRAC6|M1000e)
+		echo "Requesting CSR"
+		sh $libdir/requestssl.sh
+		echo "Signing CSR"
+		$workdir/../ca-utils/scripts/generate_ipmi_cert.sh $host
+		echo "Deploying certificate"
+		sh -x $libdir/deployssl.sh
+	;;
+	iDRAC[789])
+		$workdir/../ca-utils/scripts/generate_ipmi_cert.sh $host
+		sh -x $libdir/deployssl.sh
+		$racadm racreset
+	;;
+esac
