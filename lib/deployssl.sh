af5b4db028fffb925d36a18f3c4e9ab5bd44f123
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2022-04-29 11:45:07 +0200

    Use external certificates
    This commit leaves certificate management for iDRAC6 and M1000e broken.

--- lib/deployssl.sh
+++ lib/deployssl.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-certdir="${workdir}/../ca-utils/ipmi_certificates/"
+certdir="${workdir}/../ipmi_certificates/"
 
-KEYFILE="${certdir}/${host}_key.pem"
-CERTFILE="${certdir}/${host}_cert.pem"
-CHAINFILE="${certdir}/${host}_cert_chain.pem"
+KEYFILE="${certdir}/${host}-key.pem"
+CERTFILE="${certdir}/${host}.pem"
+CHAINFILE="${certdir}/${host}-chain.pem"
 
 if ! [ -f "$CERTFILE" ]; then
     echo "Could not find certificate file for this server!"
@@ -14,10 +14,6 @@ fi
 case "$model" in
     M1000e)
         $racadm sslcertupload -t 1 -f "$CHAINFILE"
-    ;;
-    iDRAC6-*)
-        # iDRAC6 reboots automatically but does not support chains.
-        $racadm sslcertupload -t 1 -f "$CERTFILE"
     ;;
     iDRAC[789])
         $racadm sslkeyupload -t  1 -f "$KEYFILE"
--- lib/requestssl.sh
+++ lib/requestssl.sh
@@ -29,66 +29,6 @@ EOF
         $racadm sslcsrgen -g
         $racadm sslcsrgen -f "${certdir}/${host}_csr.pem"
     ;;
-    iDRAC[789])
-        tf=$(mktemp)
-        cat > $tf << EOF
-<SystemConfiguration>
-<Component FQDD="iDRAC.Embedded.1">
- <Attribute Name="Security.1#CsrCommonName">$host</Attribute>
- <Attribute Name="Security.1#CsrOrganizationName">InnoGames GmbH</Attribute>
- <Attribute Name="Security.1#CsrOrganizationUnit">System Administration</Attribute>
- <Attribute Name="Security.1#CsrLocalityName">Hamburg</Attribute>
- <Attribute Name="Security.1#CsrStateName">Hamburg</Attribute>
- <Attribute Name="Security.1#CsrCountryCode">DE</Attribute>
- <Attribute Name="Security.1#CsrEmailAddr">it@innogames.com</Attribute>
- <Attribute Name="Security.1#CsrKeySize">2048</Attribute>
- <Attribute Name="Security.1#CsrSubjectAltName">$host</Attribute>
-</Component>
-</SystemConfiguration>
-EOF
-        $racadm set -f $tf -t xml
-        rm $tf
-        $racadm sslcsrgen -g
-        $racadm sslcsrgen -f "${certdir}/${host}_csr.pem"
-    ;;
-    iDRAC6-blade)
-        tf=$(mktemp)
-        cat << EOF > $tf
-[cfgRacSecurityData]
-cfgRacSecCsrIndex=1
-cfgRacSecCsrKeySize=2048
-cfgRacSecCsrCommonName=$host
-cfgRacSecCsrOrganizationName=InnoGames GmbH
-cfgRacSecCsrOrganizationUnit=System Administration
-cfgRacSecCsrLocalityName=Hamburg
-cfgRacSecCsrStateName=Hamburg
-cfgRacSecCsrCountryCode=DE
-cfgRacSecCsrEmailAddr=it@innogames.com
-EOF
-        $racadm config -f $tf
-        rm $tf
-        $racadm sslcsrgen -g
-        $racadm sslcsrgen -f "${certdir}/${host}_csr.pem"
-    ;;
-    iDRAC6-standalone)
-        tf=$(mktemp)
-        cat << EOF > $tf
-[cfgRacSecurity]
-cfgRacSecCsrKeySize=2048
-cfgRacSecCsrCommonName=$host
-cfgRacSecCsrOrganizationName=InnoGames GmbH
-cfgRacSecCsrOrganizationUnit=System Administration
-cfgRacSecCsrLocalityName=Hamburg
-cfgRacSecCsrStateName=Hamburg
-cfgRacSecCsrCountryCode=DE
-cfgRacSecCsrEmailAddr=it@innogames.com
-EOF
-        $racadm config -f $tf
-        rm $tf
-        $racadm sslcsrgen -g
-        sleep 60 # This shit takes a lot of time
-        $racadm sslcsrgen -f "${certdir}/${host}_csr.pem"
-    ;;
     *)
         echo "This iDRAC version can work with uploaded keys, no need for CSR"
     ;;
