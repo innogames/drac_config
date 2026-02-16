340d161fa14be3d8ad09020ff38cb308604d4e52
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2026-02-16 14:18:27 +0100

    After deploying SSL certs restart the iDRAC only when needed

--- lib/deployssl.sh
+++ lib/deployssl.sh
@@ -19,10 +19,16 @@ case "$model" in
         # iDRAC6 reboots automatically but does not support chains.
         $racadm sslcertupload -t 1 -f "$CERTFILE"
     ;;
-    iDRAC[789])
+    iDRAC[78])
+	# iDRAC 7 and 8 must be rebooted to use the new certificate.
         $racadm sslkeyupload -t  1 -f "$KEYFILE"
         $racadm sslcertupload -t 1 -f "$CHAINFILE"
         $racadm racreset
+    ;;
+    iDRAC[9])
+	# iDRAC does not need to reboot, it reboots its own web server.
+        $racadm sslkeyupload -t  1 -f "$KEYFILE"
+        $racadm sslcertupload -t 1 -f "$CHAINFILE"
     ;;
 esac
 
